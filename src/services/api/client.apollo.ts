5092ccf4f174db1c8e630f4c243d158a28a7bb11
Lucas Olveira <lucastxt@outlook.com>
2024-05-31 11:37:00 -0300

    feat: remove token

--- src/services/api/client.apollo.ts
+++ src/services/api/client.apollo.ts
@@ -1,13 +1,9 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
-const token = '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';
 const api = 'https://api.github.com/graphql';
 
 const httpLink = new HttpLink({
   uri: api,
-  headers: {
-    Authorization: `Bearer ${token}`,
-  },
 });
 
 const client = new ApolloClient({
