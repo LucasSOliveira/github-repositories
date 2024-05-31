3c42c6b6ba8ff49c664a436f015b8590f0bf59b6
Lucas Olveira <lucastxt@outlook.com>
2024-05-31 11:47:32 -0300

    feat: add new token

--- src/services/api/client.apollo.ts
+++ src/services/api/client.apollo.ts
@@ -1,9 +1,13 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
+const token = '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';
 const api = 'https://api.github.com/graphql';
 
 const httpLink = new HttpLink({
   uri: api,
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
 });
 
 const client = new ApolloClient({
