26aa8306079f8d61ed8a88358478568372aa7401
Lucas Olveira <lucastxt@outlook.com>
2024-05-31 12:18:54 -0300

    remove: console

--- src/services/api/client.apollo.ts
+++ src/services/api/client.apollo.ts
@@ -1,7 +1,6 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const token = import.meta.env.VITE_GITHUB_TOKEN;
-console.log('token', token);
 
 const httpLink = new HttpLink({
   uri: 'https://api.github.com/graphql',
